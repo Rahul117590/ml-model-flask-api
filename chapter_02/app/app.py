3dabc3c22bb0c9847e93fc64386b592ace166c26
rahul117590 <rahulkumar117590@gmail.com>
2026-07-25 03:49:20 -0700

    chapter 1 and 2 complete

---
+++ chapter_02/app/app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from main import get_all_products
+
+app=FastAPI()
+
+@app.get('/')
+def root():
+    return ('mesage')
+
+@app.get('/products')
+def products():
+    return get_all_products()
